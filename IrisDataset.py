ea399c1f5564f6c0659645a0b9b5c1e6cd75e0fd
jesp9435 <jesp9435@gmail.com>
2024-03-12 16:12:15 +0100

    Update IrisDataset.py

--- IrisDataset.py
+++ IrisDataset.py
@@ -1,5 +1,13 @@
-from sklearn import datasets
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from copy import deepcopy
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+from modAL.models import ActiveLearner, Committee
+from sklearn.decomposition import PCA
+import seaborn
+
 iris = datasets.load_iris()
 
 _, ax = plt.subplots()
@@ -8,4 +16,50 @@ ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
 _ = ax.legend(
     scatter.legend_elements()[0], iris.target_names, loc="lower right", title="Classes"
 )
-plt.show()
+#plt.show()
+
+# visualizing the classes
+with plt.style.context('seaborn-v0_8-white'):
+    plt.figure(figsize=(7, 7))
+    pca = PCA(n_components=2).fit_transform(iris['data'])
+    plt.scatter(x=pca[:, 0], y=pca[:, 1], c=iris['target'], cmap='viridis', s=50)
+    plt.title('The iris dataset')
+    plt.show()
+
+# generate the pool
+X_pool = deepcopy(iris['data'])
+y_pool = deepcopy(iris['target'])
+
+# initializing Committee members
+n_members = 2
+learner_list = list()
+
+for member_idx in range(n_members):
+    # initial training data
+    n_initial = 2
+    train_idx = np.random.choice(range(X_pool.shape[0]), size=n_initial, replace=False)
+    X_train = X_pool[train_idx]
+    y_train = y_pool[train_idx]
+
+    # creating a reduced copy of the data with the known instances removed
+    X_pool = np.delete(X_pool, train_idx, axis=0)
+    y_pool = np.delete(y_pool, train_idx)
+
+    # initializing learner
+    learner = ActiveLearner(
+        estimator=RandomForestClassifier(),
+        X_training=X_train, y_training=y_train
+    )
+    learner_list.append(learner)
+
+# assembling the committee
+committee = Committee(learner_list=learner_list)
+
+with plt.style.context('seaborn-v0_8-white'):
+    plt.figure(figsize=(n_members*7, 7))
+    for learner_idx, learner in enumerate(committee):
+        plt.subplot(1, n_members, learner_idx + 1)
+        plt.scatter(x=pca[:, 0], y=pca[:, 1], c=learner.predict(iris['data']), cmap='viridis', s=50)
+        plt.title('Learner no. %d initial predictions' % (learner_idx + 1))
+    plt.show()
+
