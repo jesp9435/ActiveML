21fd5964e8d274f1581c360644c952b6734e9f93
jesp9435 <jesp9435@gmail.com>
2024-03-13 12:55:56 +0100

    Update IrisDataset.py

--- IrisDataset.py
+++ IrisDataset.py
@@ -31,7 +31,7 @@ X_pool = deepcopy(iris['data'])
 y_pool = deepcopy(iris['target'])
 
 # initializing Committee members
-n_members = 2
+n_members = 5
 learner_list = list()
 
 for member_idx in range(n_members):
@@ -62,4 +62,65 @@ with plt.style.context('seaborn-v0_8-white'):
         plt.scatter(x=pca[:, 0], y=pca[:, 1], c=learner.predict(iris['data']), cmap='viridis', s=50)
         plt.title('Learner no. %d initial predictions' % (learner_idx + 1))
     plt.show()
+
+unqueried_score = committee.score(iris['data'], iris['target'])
+
+with plt.style.context('seaborn-v0_8-white'):
+    plt.figure(figsize=(7, 7))
+    prediction = committee.predict(iris['data'])
+    plt.scatter(x=pca[:, 0], y=pca[:, 1], c=prediction, cmap='viridis', s=50)
+    plt.title('Committee initial predictions, accuracy = %1.3f' % unqueried_score)
+    plt.show()
+
+performance_history = [unqueried_score]
+
+# query by committee
+n_queries = 20
+for idx in range(n_queries):
+    query_idx, query_instance = committee.query(X_pool)
+    committee.teach(
+        X=X_pool[query_idx].reshape(1, -1),
+        y=y_pool[query_idx].reshape(1, )
+    )
+    performance_history.append(committee.score(iris['data'], iris['target']))
+    # remove queried instance from pool
+    X_pool = np.delete(X_pool, query_idx, axis=0)
+    y_pool = np.delete(y_pool, query_idx)
+
+# visualizing the final predictions per learner
+with plt.style.context('seaborn-v0_8-white'):
+    plt.figure(figsize=(n_members*7, 7))
+    for learner_idx, learner in enumerate(committee):
+        plt.subplot(1, n_members, learner_idx + 1)
+        plt.scatter(x=pca[:, 0], y=pca[:, 1], c=learner.predict(iris['data']), cmap='viridis', s=50)
+        plt.title('Learner no. %d predictions after %d queries' % (learner_idx + 1, n_queries))
+    plt.show()
+
+# visualizing the Committee's predictions
+with plt.style.context('seaborn-v0_8-white'):
+    plt.figure(figsize=(7, 7))
+    prediction = committee.predict(iris['data'])
+    plt.scatter(x=pca[:, 0], y=pca[:, 1], c=prediction, cmap='viridis', s=50)
+    plt.title('Committee predictions after %d queries, accuracy = %1.3f'
+              % (n_queries, committee.score(iris['data'], iris['target'])))
+    plt.show()
+
+# Plot our performance over time.
+fig, ax = plt.subplots(figsize=(8.5, 6), dpi=130)
+
+ax.plot(performance_history)
+ax.scatter(range(len(performance_history)), performance_history, s=13)
+
+ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(nbins=5, integer=True))
+ax.yaxis.set_major_locator(mpl.ticker.MaxNLocator(nbins=10))
+ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1))
+
+ax.set_ylim(bottom=0, top=1)
+ax.grid(True)
+
+ax.set_title('Incremental classification accuracy')
+ax.set_xlabel('Query iteration')
+ax.set_ylabel('Classification Accuracy')
+
+plt.show()
 
