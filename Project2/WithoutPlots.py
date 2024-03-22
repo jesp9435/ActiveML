fdc5fb7b52ed61d462ef672213312d11eec32559
CRisager <chelina.risager@gmail.com>
2024-03-22 10:21:19 +0100

    Update WithoutPlots.py

--- Project2/WithoutPlots.py
+++ Project2/WithoutPlots.py
@@ -8,7 +8,8 @@ from sklearn.ensemble import RandomForestClassifier
 from modAL.models import ActiveLearner, Committee
 from sklearn.decomposition import PCA
 import random
-import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import sem
 
 
 # Load the data set
@@ -71,7 +72,7 @@ members10_acc = []
 members15_acc = []
 
 
-#repeat the whole experiment 10 time to achieve confidence intervals
+#repeat the whole experiment 10 times to achieve confidence intervals
 for repetition in range(10): 
     # loop through the number of committee members
     for n in [5, 10, 15]:
@@ -152,5 +153,110 @@ for repetition in range(10):
 
 
 
+total_acc = [members5_acc, members10_acc, members15_acc]
+
+
 
 ### Plot our performance over time. 
+
+# initialize a list for storing average and 95% confidence interval
+average_data = [[],[],[]] # on the form [members5, members10, members15]
+
+# loop over the committee sizes
+for n in range(3): # loop over number of committee sizes (5, 10, 15)
+    datalist = total_acc[n] # determine the list to extract data from
+    appendlist = average_data[n] # determine the list to append to
+    
+    for i in range(4):
+        # Calculate the mean and standard error for each method
+        method_data = [row[i] for row in datalist]
+        mean_method_data = np.mean(method_data, axis=0)
+        #mean_method_data = [num+20 for num in mean_method_data if num > 50 and num < 80]
+        sem_method_data = sem(method_data, axis=0)
+        appendlist.append([mean_method_data, sem_method_data])
+    
+# Create x values
+x = np.arange(1, 11)
+
+for i in range(3):# loop over number of committee sizes (5, 10, 15)
+    member_num = average_data[i]
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    for method in range(len(member_num)):
+        if method == 0:
+            plt.plot(x, member_num[method][0], label="Vote Entropy", marker='o')
+        elif method == 1:
+            plt.plot(x, member_num[method][0], label="KLD", marker='o')
+        elif method == 2:
+            plt.plot(x, member_num[method][0], label="Consensus", marker='o')
+        else:
+            plt.plot(x, member_num[method][0], label="Baseline", marker='o')
+        plt.fill_between(x, member_num[method][0] - 1.96 * member_num[method][1], member_num[method][0] + 1.96 * member_num[method][1], alpha=0.2)
+
+    plt.xlabel("Query ID")
+    plt.ylim(0,100)
+    plt.ylabel("Accuracy in %")
+    plt.title("Committee size " + str([5, 10, 15][i]))
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+### plot the best performing against eachother (only for us)
+plt.figure(figsize=(10, 6))
+# Best of 5 members
+plt.plot(x, average_data[0][1][0], label="5 members KLD", marker='o') #average_data[0][1][0] = [5 members][KLD][mean]
+plt.fill_between(x, average_data[0][1][0] - 1.96 * average_data[0][1][1], average_data[0][1][0] + 1.96 * average_data[0][1][1], alpha=0.2)
+# Best of 10 members
+plt.plot(x, average_data[1][0][0], label="10 members vote entropy", marker='o') 
+plt.fill_between(x, average_data[1][0][0] - 1.96 * average_data[1][0][1], average_data[1][0][0] + 1.96 * average_data[1][0][1], alpha=0.2)
+# Best of 15 members
+plt.plot(x, average_data[2][2][0], label="15 members consensus prob", marker='o') 
+plt.fill_between(x, average_data[2][2][0] - 1.96 * average_data[2][2][1], average_data[2][2][0] + 1.96 * average_data[2][2][1], alpha=0.2)
+
+plt.xlabel("Query ID")
+plt.ylim(0,100)
+plt.ylabel("Accuracy in %")
+plt.title("The best performing")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+### plot the baselines against eachother (only for us)
+plt.figure(figsize=(10, 6))
+# Best of 5 members
+plt.plot(x, average_data[0][3][0], label="5 members", marker='o') #average_data[0][1][0] = [5 members][KLD][mean]
+plt.fill_between(x, average_data[0][3][0] - 1.96 * average_data[0][3][1], average_data[0][3][0] + 1.96 * average_data[0][3][1], alpha=0.2)
+# Best of 10 members
+plt.plot(x, average_data[1][3][0], label="10 members", marker='o') 
+plt.fill_between(x, average_data[1][3][0] - 1.96 * average_data[1][3][1], average_data[1][3][0] + 1.96 * average_data[1][3][1], alpha=0.2)
+# Best of 15 members
+plt.plot(x, average_data[2][3][0], label="15 members", marker='o') 
+plt.fill_between(x, average_data[2][3][0] - 1.96 * average_data[2][3][1], average_data[2][3][0] + 1.96 * average_data[2][3][1], alpha=0.2)
+
+plt.xlabel("Query ID")
+plt.ylim(0,100)
+plt.ylabel("Accuracy in %")
+plt.title("Baselines")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+
+### plot the best combo against the best baseline
+plt.figure(figsize=(10, 6))
+# Best combo
+plt.plot(x, average_data[0][1][0], label="5 members KLD", marker='o') 
+plt.fill_between(x, average_data[0][1][0] - 1.96 * average_data[0][1][1], average_data[0][1][0] + 1.96 * average_data[0][1][1], alpha=0.2)
+# best performing baseline
+plt.plot(x, average_data[1][3][0], label="10 members baseline", marker='o') 
+plt.fill_between(x, average_data[1][3][0] - 1.96 * average_data[1][3][1], average_data[1][3][0] + 1.96 * average_data[1][3][1], alpha=0.2)
+plt.xlabel("Query ID")
+plt.ylim(0,100)
+plt.ylabel("Accuracy in %")
+plt.title("Best vs baseline")
+plt.legend()
+plt.grid(True)
+plt.show()
