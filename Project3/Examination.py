80d2873be525c2e73977203d31d7ab38d32a1f23
CRisager <chelina.risager@gmail.com>
2024-04-23 16:40:25 +0200

    a lot

--- Project3/Examination.py
+++ Project3/Examination.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KernelDensity
 import seaborn as sns
 import math
 import os
+
+np.random.seed(42)
 
 os.chdir("C:/Users/cheli/Downloads")
 
@@ -22,10 +25,29 @@ del data4[data4.columns[0]]
 data5 = pd.read_csv("data_1312.csv") # B = 0
 del data5[data5.columns[0]]
 
-data = data5
-# plot all variables against eachother
+data6 = pd.read_csv("data_1361.csv") # D = -2
+del data6[data6.columns[0]]
+
+# Choose the data to investigate
+data = data6
+
+###################################################
+## plot all variables against eachother ##
 sns.pairplot(data, plot_kws={"s": 5})
 plt.show()
+
+# Data1: The relationship between A & F seems quite linear
+#        aka with a clear correlation. Furthermore E & F 
+#        seems to have a parabolic relationship, and D & E 
+#        creates an "H" shape and lastly C & E seems to 
+#        create a square.
+# Data2/F: D is almost constant at 0 when we intervene with F=0
+# Data3/E: No clear relationships
+# Data4/C: No clear relationships ish
+# Data5/B: Once more D & E creates an "H" shape and E & F 
+#        seems to have a parabolic relationship. Furthermore 
+#        C and D seems to perhaps be correlated 
+# Data6/D: The C & E square is gone 
 
 
 ###################################################
@@ -42,11 +64,13 @@ for row in corrM:
             print("var",row,"and var",column, "has corr:",value)
 
 # Data1: var A and var F has corr: -0.5407078849279002
-# Data2: none
-# Data3: none
-# Data4: var A and var F has corr: -0.7077970481544589 
-# Data3: none
-
+# Data2/F: none
+# Data3/E: none
+# Data4/C: var A and var F has corr: -0.7077970481544589 
+# Data5/B: none
+# Data6/D: var A and var F has corr: -0.6580983545598529
+#          var B and var C has corr: 0.6450769014297322
+#          var B and var F has corr: 0.5375220107260147
 
 
 ###################################################
@@ -54,6 +78,7 @@ for row in corrM:
 
 # Function for calculating mutual information
 # and for plotting if wanted
+"""
 def MI(x,y,Nbins=21, plot = False):
     bins = np.linspace(np.min(x),np.max(x),Nbins)
     eps=np.spacing(1)
@@ -75,31 +100,66 @@ def MI(x,y,Nbins=21, plot = False):
         plt.suptitle('Mutual information: %f'%MI)
         plt.show()
     return(MI)
+"""
 
+def MI(x, y, bandwidth=0.1, plot=False):
+    # Convert x and y to np arrays
+    x = np.array(x)
+    y = np.array(y)
+    # Fit KDE to X and Y
+    kde_x = KernelDensity(bandwidth=bandwidth).fit(x.reshape(-1, 1))
+    kde_y = KernelDensity(bandwidth=bandwidth).fit(y.reshape(-1, 1))
+    # Evaluate KDE at sample points
+    log_density_x = kde_x.score_samples(x.reshape(-1, 1))
+    log_density_y = kde_y.score_samples(y.reshape(-1, 1))
+    # Compute joint KDE
+    joint_kde = np.exp(log_density_x + log_density_y)
+    # Compute marginal KDE
+    marginal_kde_x = np.exp(log_density_x)
+    marginal_kde_y = np.exp(log_density_y)
+    # Compute MI
+    eps = np.spacing(1)
+    MI = np.sum(joint_kde * np.log((joint_kde + eps) / (marginal_kde_x * marginal_kde_y + eps)))
+    if plot:
+        plt.figure(figsize=(10, 4))
+        plt.subplot(1, 2, 1)
+        plt.scatter(x, y, alpha=0.2)
+        plt.title("Joint Distribution")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.subplot(1, 2, 2)
+        plt.scatter(x, marginal_kde_x, label="P(X)", alpha=0.5)
+        plt.scatter(y, marginal_kde_y, label="P(Y)", alpha=0.5)
+        plt.title("Product of Marginals")
+        plt.xlabel("X / Y")
+        plt.ylabel("Density")
+        plt.legend()
+        plt.suptitle(f"Mutual Information: {MI:.4f}")
+        plt.show()
+    return MI
 
 # create list for storing all MI values
 datapoints = []
 # Calculate all MI values for all pairs of variables
 for row in data:
     for column in data.columns:
-        if row != column:
+        if row != column and MI(data[str(row)], data[str(column)]) not in datapoints:
             datapoints.append(MI(data[str(row)], data[str(column)]))
             print(row, " and ", column, " has MI: ", MI(data[str(row)], data[str(column)]))
-
-# Make sure there is no nan values by replacing them with 0
-datapoints = [n for n in datapoints if not isinstance(n, float) or not math.isnan(n)]
 
 # Create boxplot to investigate if there are any MI value outliers
 # This would indicate a particually strong/weak relationship between the given variables
 plt.boxplot(datapoints, labels = [""])
 plt.scatter(np.ones_like(datapoints), datapoints, color='red', marker='o', alpha=0.5)
 plt.ylabel("MI values")  
-plt.show()
-#Data1: from the plotted boxplot we do not get any outliers e.i., MI tells us nothing
-#Data2: There is one outlier of an encredible low value between C and F 
-#Data3: There is one outlier of an encredible low value between A and E
-#Data4: There are two outliers: A and C as well as B and C
-#Data5: No outliers, but low: C & B, D & B, E & B, F & B 
+plt.show() 
+# Data1:   Positive outlier = B & D
+# Data2/F: Outlier = D & F
+# Data3/E: Outliers = C & E and D & E
+# Data4/C: Outlier = C & F
+# Data5/B: Outliers = B & D and B & E
+# Data6/D: Positive outliers = D & E and D & F
+#          Negative outliers = B & D and B & E and C & F
 
 
 ###################################################
@@ -118,7 +178,7 @@ print(min(data["F"])) # 4.9873
 
 #### After data2:
 # We want to examine which variables changed distribution
-for variable in data:
+for variable in data1:
     plt.boxplot(data1[variable], positions=[1], labels = [variable + "1"])
     plt.boxplot(data2[variable], positions=[2], labels = [variable + "2"])
     plt.scatter(np.ones_like(data1[variable]), data1[variable], color='blue', marker='o', alpha=0.5)
@@ -131,7 +191,7 @@ for variable in data:
 # D changed
 # E changed but...
 
-# We now want to sample E (because we don't know a lot about it)
+# We now want to sample E (because it has a few possible connections)
 # and want to try and use a negative value this time. 
 # So we use E=-2, as this is the most extreme we're allowed to
 
@@ -139,7 +199,7 @@ for variable in data:
 
 #### After data3:
 # We want to examine which variables changed distribution
-for variable in data:
+for variable in data1:
     plt.boxplot(data1[variable], positions=[1], labels = [variable + "1"])
     plt.boxplot(data3[variable], positions=[2], labels = [variable + "2"])
     plt.scatter(np.ones_like(data1[variable]), data1[variable], color='blue', marker='o', alpha=0.5)
@@ -203,3 +263,17 @@ for variable in data:
 
 
 
+#### After data6:
+# We want to examine which variables changed distribution
+for variable in data:
+    plt.boxplot(data1[variable], positions=[1], labels = [variable + "1"])
+    plt.boxplot(data6[variable], positions=[2], labels = [variable + "2"])
+    plt.scatter(np.ones_like(data1[variable]), data1[variable], color='blue', marker='o', alpha=0.5)
+    plt.scatter(2 * np.ones_like(data6[variable]), data6[variable], color='red', marker='x', alpha=0.5)
+    plt.ylabel("Values")  
+    plt.show()
+# A 
+# C 
+# D 
+# E 
+# F 
